59c7451d68673a59e9cd7ac66d1403b13b89c67f
Amira Baati <amirabaati@gmail.com>
2021-11-15 11:32:55 +0100

    service+controller

---
+++ LibraryManagerWebAPI/Service/Bookservice.cs
@@ -0,0 +1,41 @@
+﻿using DAOLibrary;
+using DAOLibrary.Model;
+using System;
+using System.Collections.Generic;
+
+public class Bookservice
+{
+	private IDAOBook daobook;
+	public Bookservice(IDAOBook daobook)
+	{
+		this.daobook = daobook;
+	}
+	public void Add(String title, int quantity, String category)
+	{
+		Book book = new Book(title, quantity, category);
+		daobook.Add(book);
+	}
+	public void Update(String id)
+	{
+		daobook.Update(id);
+	}
+	public void Delete(String id)
+	{
+		daobook.Delete(id);
+	}
+	public IEnumerable<Book> FindById(string id)
+	{
+		return daobook.FindById(id);
+	}
+	public IEnumerable<Book> FindByTitle(String title)
+	{
+		return daobook.FindByTitle(title);
+	}
+	public IEnumerable<Book> FindByCategory(String category)
+	{
+		return daobook.FindByTitle(category);
+
+	}
+
+
+}
